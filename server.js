42b9489f76e093066a9e7bc71f57e4f3c5677186
Marlon Gamba <marlonpgamba@gmail.com>
2017-09-18 20:53:52 -0300

    tst

--- server.js
+++ server.js
@@ -1,132 +1,19 @@
-// BASE SETUP
-// =============================================================================
+var express = require('express');
+var app = express();
 
-// call the packages we need
-var express    = require('express');
-var bodyParser = require('body-parser');
-var app        = express();
-
-// configure body parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var ip = process.env.OPENSHIFT_NODEJS_IP;
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-if (typeof ipaddress === "undefined") {
+if (typeof ip === "undefined") {
     //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
     //  allows us to run/test the app locally.
     console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-    ipaddress = "127.0.0.1";
+    ip = "127.0.0.1";
 };
 
-var mongoose   = require('mongoose');
-mongoose.connect('mongodb://OPENSHIFT_MONGODB_DB_HOST:OPENSHIFT_MONGODB_DB_PORT/appapi'); // connect to our database
-var Bear     = require('./app/models/bear');
-
-// ROUTES FOR OUR API
-// =============================================================================
-
-// create our router
-var router = express.Router();
-
-// middleware to use for all requests
-router.use(function(req, res, next) {
-	// do logging
-	console.log('Something is happening.');
-	next();
+app.get('/', function (req, res) {
+	res.send('Hello World');
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });	
-});
-
-// on routes that end in /bears
-// ----------------------------------------------------
-router.route('/bears')
-
-	// create a bear (accessed at POST http://localhost:8080/bears)
-	.post(function(req, res) {
-		
-		var bear = new Bear();		// create a new instance of the Bear model
-		bear.name = req.body.name;  // set the bears name (comes from the request)
-
-		bear.save(function(err) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'Bear created!' });
-		});
-
-		
-	})
-
-	// get all the bears (accessed at GET http://localhost:8080/api/bears)
-	.get(function(req, res) {
-		Bear.find(function(err, bears) {
-			if (err)
-				res.send(err);
-
-			res.json(bears);
-		});
-	});
-
-// on routes that end in /bears/:bear_id
-// ----------------------------------------------------
-router.route('/bears/:bear_id')
-
-	// get the bear with that id
-	.get(function(req, res) {
-		Bear.findById(req.params.bear_id, function(err, bear) {
-			if (err)
-				res.send(err);
-			res.json(bear);
-		});
-	})
-
-	// update the bear with this id
-	.put(function(req, res) {
-		Bear.findById(req.params.bear_id, function(err, bear) {
-
-			if (err)
-				res.send(err);
-
-			bear.name = req.body.name;
-			bear.save(function(err) {
-				if (err)
-					res.send(err);
-
-				res.json({ message: 'Bear updated!' });
-			});
-
-		});
-	})
-
-	// delete the bear with this id
-	.delete(function(req, res) {
-		Bear.remove({
-			_id: req.params.bear_id
-		}, function(err, bear) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'Successfully deleted' });
-		});
-	});
-
-router.route('/test').get(function(req, res) {
-		res.json("OK");
-	});
-
-// REGISTER OUR ROUTES -------------------------------
-app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
-app.listen(port, ipaddress, function() {
-     console.log('%s: Node server started on %s:%d ...',
-    Date(Date.now() ), ipaddress, port);
-});
-console.log('Magic happens on port ' + port);
+app.listen(port, ip);
+console.log("App listening at http://%s:%s", ip, port);
