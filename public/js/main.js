1253fa367ae149352908aec62c86c61f1182d0fe
Marlon Gamba <marlonpgamba@gmail.com>
2017-09-19 00:48:01 -0300

    Adding mocked endpoints to test my app

--- public/js/main.js
+++ public/js/main.js
@@ -8,30 +8,38 @@ function initMenu(){
     document.getElementById('leftnav').setAttribute("w3-include-html", "menu.html");
 	w3.includeHTML();
 }
-function initSearch(){
+function navigateTotSearch(){
     console.log("initSearch");
     document.getElementById('main-content').setAttribute("w3-include-html", "search.html");
 	w3.includeHTML();
 }
 
-function initTimeLine(){
+function navigateToTimeLine(){
     console.log("initTimeLine");
     document.getElementById('main-content').setAttribute("w3-include-html", "timeline.html");
-	w3.includeHTML();
+    w3.includeHTML();
+    initTimeLine();
 }
 
-function initProductRegister(){
+function navigateToProductRegister(){
     console.log("initProductRegister");
     document.getElementById('main-content').setAttribute("w3-include-html", "product-register.html");
 	w3.includeHTML();
 }
 
+function initTimeLine(){
+    $('#search') = '';
+    var result = getProducts();
+    var list = $('#timelineResult');
+    populateList(list, result);
+}
 
 function search(){
     var result = getProducts();
     console.log(result);
-    $('#result').empty();
-    populateList(result);
+    var list = $('#result');
+    list.empty();
+    populateList(list, result);
 }
 
 function getProducts() {
@@ -43,10 +51,10 @@ function getProducts() {
     });
 }
 
-function populateList(data){
+function populateList(list, data){
     var items = [];
     $.each(data, function(i, item) {
         items.push('<li><div class="img" style="background-image:url(\'img' + item.imgurl+ '\');"></div><div class="result-text"><a href="#" class="text-align">' + item.name + '</a><br><span class="text-align">' + item.description + '</span></div></li>');
     });
-    $('#result').append( items.join('') );
+    list.append( items.join('') );
 }
