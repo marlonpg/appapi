578b85b38b17a13ad6369f2b3c7280fa0abe9998
Marlon Gamba <marlonpgamba@gmail.com>
2017-09-18 22:52:14 -0300

    Adding mocked endpoints to test my app

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
-var express = require('express'),
-app = express(); 
+var express = require('express');
+var app = express();
+
+var ip = process.env.OPENSHIFT_NODEJS_IP;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+if (typeof ip === "undefined") {
+    //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+    //  allows us to run/test the app locally.
+    console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+    ip = "127.0.0.1";
+};
+
+
+
 app.use('/', express.static('public/'));
-app.listen(8080);
+
+app.listen(port, ip);
+console.log("App listening at http://%s:%s", ip, port);
+
+
