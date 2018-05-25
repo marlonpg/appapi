c988c484d6e20b3040512811cd3aae5f10ce25e5
mgamba <marlonpgamba@gmail.com>
2018-05-24 21:52:51 -0300

    Translate some words to portuguese.

--- models/user.js
+++ models/user.js
@@ -1,8 +1,7 @@
-// get an instance of mongoose and mongoose.Schema
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
-// set up a mongoose model and pass it using module.exports
+
 module.exports = mongoose.model('User', new Schema({ 
 	name: String, 
     email: String, 
