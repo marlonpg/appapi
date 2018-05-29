919bedab711758022e6430b1da93086e9b4f7bd9
mgamba <marlonpgamba@gmail.com>
2018-05-28 22:35:24 -0300

    Adding openshift configuration

--- server.js
+++ server.js
@@ -12,14 +12,31 @@ var DesiredProduct   = require('./models/desiredproduct');
 
 ///////////////////
 //CONFIGURATION
-var ip = process.env.OPENSHIFT_NODEJS_IP;
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
 
-if (typeof ip === "undefined") {
-  console.warn("No OPENSHIFT_NODEJS_IP var, using 127.0.0.1");
-  ip = "127.0.0.1";
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+  }
 }
-mongoose.connect(config.database);
+mongoose.connect(mongoURL);
 app.set('secret', config.secret);
 
 //ACCESS POST body information and URL parameters
